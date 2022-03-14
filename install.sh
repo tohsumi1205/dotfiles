44e855d5bad943fae8ee62c6341ac9a9e2f4f0ff
tohsumi <tohsumi@student.42tokyo.jp>
2022-03-14 11:23:31 +0900

    add install options

--- install.sh
+++ install.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 
+# todo
+#  install bat ripgrep tmux vim(latest)
+
 set -u
 
 git submodule update --init --recursive
 
 for f in .??*; do
 	[ "$f" = ".git" ] && continue
+	[ "$f" = ".gitmodules" ] && continue
 	
 	ln -snfv ${PWD}/"$f" ~
 done
+
+# install vim plugins
+vim -c PackUpdate -c wq ~/.vimrc
+
+~/.vim/pack/minpac/start/fzf/install
